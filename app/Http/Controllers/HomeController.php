a1198605ef59262293ed82321b15c746b293d887
AddilAfzal <aadil01@hotmail.co.uk>
2017-07-02 19:46:45 +0100

    Support for JSON

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,17 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index() {
-    	return view('home.index');
-	}
+		$servers = json_decode(file_get_contents(storage_path() . "/servers.json"), true);
 
+		$GameQ = new \GameQ\GameQ();
+		$GameQ->addServer([
+			'type' => 'css',
+			'host' => '127.0.0.1:27015',
+		]);
+
+		$results = $GameQ->process();
+		
+		return view('home.index')
+			->with('servers', $servers);
+	}
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -20,36 +20,40 @@
             </thead>
 
             <tbody>
-
-            <tr>
-                <td>
-                    <span class="badge badge-success"> Online</span>
-                </td>
-                <td width="10px">
-                    <img src="/img/servers/game_icons/sm/cs16.gif"
-                         alt="Counter Strike 1.6">
-                </td>
-                <td>
-                    148.251.78.214:27017
-                </td>
-                <td>
-                    Assault . hsCorp . de [JETPACK|RESPAWN|CSDM|FUN]
-                </td>
-                <td>
-                    cs_assault_blizzard_final
-                </td>
-                <td>
-                    27 / 31
-                </td>
-                <td>
-                    <a class="btn btn-primary btn-sm" href="steam://connect/148.251.78.214:27017/" ><i class="fa fa-external-link" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="Connect"></i></a>
-                    <a class="btn btn-secondary btn-sm" href="/servers/assault"><i class="fa fa-bars" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="View"></i></a>
-
-                </td>
-            </tr>
+            @foreach($servers as $server)
+                <tr>
+                    <td>
+                        <span class="badge badge-success"> Online</span>
+                    </td>
+                    <td width="10px">
+                        <img src="/img/game_icons/sm/{{$server['type']}}.gif" alt="{{$server['type']}}">
+                    </td>
+                    <td>
+                         {{$server['ip']}}:{{$server['port']}}
+                    </td>
+                    <td>
+                        {{$server['name']}}
+                    </td>
+                    <td>
+                        cs_assault_blizzard_final
+                    </td>
+                    <td>
+                        27 / 31
+                    </td>
+                    <td>
+                        <a class="btn btn-primary btn-sm" href="steam://connect/148.251.78.214:27017/"><i
+                                    class="fa fa-external-link" aria-hidden="true" data-toggle="tooltip"
+                                    data-placement="top" title="Connect"></i></a>
+                        <a class="btn btn-secondary btn-sm" href="/servers/assault"><i class="fa fa-bars"
+                                                                                       aria-hidden="true"
+                                                                                       data-toggle="tooltip"
+                                                                                       data-placement="top"
+                                                                                       title="View"></i></a>
+                    </td>
+                </tr>
+            @endforeach
 
             </tbody>
-
         </table>
 
     </div>
